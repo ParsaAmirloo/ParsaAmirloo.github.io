3bb26715c5479d805a78dd60fbc1c270f9e0023d
ParsaAmirloo <160532124+ParsaAmirloo@users.noreply.github.com>
2024-04-12 20:14:46 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// Replace 'YOUR_BOT_TOKEN' with your actual bot token
+const token = '6464825264:AAE20MCPTCHGdNBPf4qAMkB_FBzMcY9QEhQ';
+
+// Create a bot instance
+const bot = new TelegramBot(token, { polling: true });
+
+// Handle '/playgame' command
+bot.onText(/\/playgame/, (msg) => {
+    // Replace 'YOUR_GAME_URL' with the URL of your HTML5 game
+    const gameUrl = 'https://parsaamirloo.github.io./';
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `Click the link to play the game: ${gameUrl}`);
+});
+
+
 function change() {
     const coinElement = document.getElementById("coin");
     coinElement.classList.add("bigger"); // Add the 'bigger' class
